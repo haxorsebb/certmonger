c68f49ba11932223117c2b6485556864f9865735
Nalin Dahyabhai <nalin.dahyabhai@pobox.com>
2009-07-14 15:42:22 -0400

    - add more interfaces

--- src/iterate.h
+++ src/iterate.h
@@ -2,10 +2,12 @@
 #define iterate_h
 
 /* Figure out what to do next about this specific entry. */
+int cm_iterate_init(struct cm_store_entry *entry, void **cm_iterate_state);
 int cm_iterate(struct cm_store_entry *entry,
-	       void **cm_iterate_state,
+	       void *cm_iterate_state,
 	       int *readfd,
 	       int *immediate,
 	       struct timeval *delay);
+int cm_iterate_done(struct cm_store_entry *entry, void *cm_iterate_state);
 
 #endif
---
+++ src/notify.h
@@ -0,0 +1,6 @@
+#ifndef notify_h
+#define notify_h
+
+int cm_notify(struct cm_store_entry *entry, time_t ttl);
+
+#endif
