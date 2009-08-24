2c7d1e9c45ae56df6eeec7189a8b749dde02619e
Nalin Dahyabhai <nalin.dahyabhai@pobox.com>
2009-08-24 18:20:05 -0400

    - docs say we don't need to manually run autoheader, either

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,5 @@
 CFLAGS=`rpm --eval '%{optflags} -Wall -Wextra -Wno-unused-parameter -g3 -O0'`
 export CFLAGS
 set -x
-autoheader
-autoreconf -i
+autoreconf -i -f
 ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --enable-maintainer-mode "$@"
