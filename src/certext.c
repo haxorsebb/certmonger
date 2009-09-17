86b5ccde3c39448a63be5055b87a178c2c96379a
Nalin Dahyabhai <nalin.dahyabhai@pobox.com>
2009-09-17 16:33:55 -0400

    - parse subjectalternativename extensions

--- src/certext.c
+++ src/certext.c
@@ -35,10 +35,151 @@
 
 #include <talloc.h>
 
+#include <krb5.h>
+
 #include "certext.h"
 #include "log.h"
 #include "store.h"
 #include "store-int.h"
+
+/* Structures and templates for parsing principal name otherName values. */
+struct realm {
+	SECItem name;
+};
+struct principal_name {
+	SECItem name_type;
+	SECItem **name_string;
+};
+struct kerberos_principal_name {
+	struct realm realm;
+	struct principal_name principal_name;
+};
+
+/* KerberosString: RFC 4120, 5.2.1 */
+static const SEC_ASN1Template
+cm_kerberos_string_template[] = {
+	{
+	.kind = SEC_ASN1_GENERAL_STRING,
+	.offset = 0,
+	.sub = NULL,
+	.size = sizeof(SECItem),
+	},
+};
+
+/* Realm == KerberosString: RFC 4120, 5.2.2 */
+static const SEC_ASN1Template
+cm_realm_template[] = {
+	{
+	.kind = SEC_ASN1_GENERAL_STRING,
+	.offset = 0,
+	.sub = NULL,
+	.size = sizeof(SECItem),
+	},
+};
+
+static const SEC_ASN1Template
+cm_sequence_of_kerberos_string_template[] = {
+	{
+	.kind = SEC_ASN1_SEQUENCE_OF,
+	.offset = 0,
+	.sub = &cm_kerberos_string_template,
+	.size = 0,
+	},
+};
+
+/* PrincipalName: RFC 4120, 5.2.2 */
+static const SEC_ASN1Template
+cm_principal_name_template[] = {
+	{
+	.kind = SEC_ASN1_SEQUENCE,
+	.offset = 0,
+	.sub = NULL,
+	.size = sizeof(struct principal_name),
+	},
+	{
+	.kind = SEC_ASN1_CONTEXT_SPECIFIC | 0 |
+	SEC_ASN1_CONSTRUCTED |
+	SEC_ASN1_EXPLICIT,
+	.offset = offsetof(struct principal_name, name_type),
+	.sub = &SEC_IntegerTemplate,
+	.size = sizeof(SECItem),
+	},
+	{
+	.kind = SEC_ASN1_CONTEXT_SPECIFIC | 1 |
+	SEC_ASN1_CONSTRUCTED |
+	SEC_ASN1_EXPLICIT,
+	.offset = offsetof(struct principal_name, name_string),
+	.sub = cm_sequence_of_kerberos_string_template,
+	.size = sizeof(struct SECItem**),
+	},
+	{0, 0, NULL, 0},
+};
+
+/* KRB5PrincipalName: RFC 4556, 3.2.2 */
+const SEC_ASN1Template
+cm_kerberos_principal_name_template[] = {
+	{
+	.kind = SEC_ASN1_SEQUENCE,
+	.offset = 0,
+	.sub = NULL,
+	.size = sizeof(struct kerberos_principal_name),
+	},
+	{
+	.kind = SEC_ASN1_CONTEXT_SPECIFIC | 0 |
+	SEC_ASN1_CONSTRUCTED |
+	SEC_ASN1_EXPLICIT,
+	.offset = offsetof(struct kerberos_principal_name, realm),
+	.sub = &cm_realm_template,
+	.size = sizeof(struct realm),
+	},
+	{
+	.kind = SEC_ASN1_CONTEXT_SPECIFIC | 1 |
+	SEC_ASN1_CONSTRUCTED |
+	SEC_ASN1_EXPLICIT,
+	.offset = offsetof(struct kerberos_principal_name, principal_name),
+	.sub = &cm_principal_name_template,
+	.size = sizeof(struct principal_name),
+	},
+	{0, 0, NULL, 0},
+};
+
+static SEC_ASN1Template
+cm_ms_upn_name_template[] = {
+	{
+	.kind = SEC_ASN1_CONTEXT_SPECIFIC | 0 |
+		SEC_ASN1_EXPLICIT |
+		SEC_ASN1_CONSTRUCTED,
+	.offset = 0,
+	.sub = SEC_UTF8StringTemplate,
+	.size = sizeof(SECItem),
+	},
+};
+
+/* Windows 2000-style UPN */
+static unsigned char oid_ms_upn_name_bytes[] = {0x2b, 0x06, 0x01, 0x04, 0x01, 0x82, 0x37, 0x14, 0x02, 0x03};
+static const SECOidData oid_ms_upn_name = {
+	.oid = {
+		.data = oid_ms_upn_name_bytes,
+		.len = 10,
+	},
+	.offset = 0,
+	.desc = "Microsoft Windows User Principal Name",
+	.mechanism = 0,
+	.supportedExtension = UNSUPPORTED_CERT_EXTENSION,
+};
+
+/* pkinit-SAN 1.3.6.1.5.2.2 */
+static unsigned char oid_pkinit_san_bytes[] = {0x2b, 0x06, 0x01, 0x05, 0x02, 0x02};
+static const SECOidData oid_pkinit_san = {
+	.oid = {
+		.data = oid_pkinit_san_bytes,
+		.len = 6,
+	},
+	.offset = 0,
+	.desc = "PKINIT Subject Alternate Name",
+	.mechanism = 0,
+	.supportedExtension = UNSUPPORTED_CERT_EXTENSION,
+};
 
 static void
 cm_certext_read_ku(struct cm_store_entry *entry, PLArenaPool *arena,
@@ -88,26 +229,134 @@ cm_certext_read_eku(struct cm_store_entry *entry, PLArenaPool *arena,
 	}
 }
 
+static char *
+cm_certext_parse_principal(void *parent, struct kerberos_principal_name *p)
+{
+	SECItem **comps;
+	krb5_context ctx;
+	krb5_principal_data princ;
+	char *unparsed, *ret;
+	int i, j;
+	unsigned long name_type;
+	ret = NULL;
+	ctx = NULL;
+	if (krb5_init_context(&ctx) == 0) {
+		memset(&princ, 0, sizeof(princ));
+		/* Copy the realm over. */
+		princ.realm.length = p->realm.name.len;
+		princ.realm.data = (char *) p->realm.name.data;
+		/* Count the number of name components. */
+		comps = p->principal_name.name_string;
+		for (i = 0; (comps != NULL) && (comps[i] != NULL); i++) {
+			continue;
+		}
+		/* Set the number of name components. */
+		princ.length = i;
+		/* Allocate and populate the name components. */
+		princ.data = talloc_zero_array(parent, krb5_data, i);
+		if (princ.data != NULL) {
+			for (j = 0; j < i; j++) {
+				princ.data[j].length = comps[j]->len;
+				princ.data[j].data = (char *) comps[j]->data;
+			}
+		}
+		/* Try to decode the name type. */
+		if (SEC_ASN1DecodeInteger(&p->principal_name.name_type,
+					  &name_type) != SECSuccess) {
+			/* Try to decode the name type. */
+			name_type = KRB5_NT_UNKNOWN;
+		}
+		princ.type = name_type;
+		/* Convert that into a string. */
+		if (krb5_unparse_name(ctx, &princ, &unparsed) == 0) {
+			ret = talloc_strdup(parent, unparsed);
+			krb5_free_unparsed_name(ctx, unparsed);
+		}
+		talloc_free(princ.data);
+		krb5_free_context(ctx);
+	}
+	return ret;
+}
+
+static void
+cm_certext_read_other_name(struct cm_store_entry *entry, PLArenaPool *arena,
+			   CERTGeneralName *name)
+{
+	SECItem *item, upn;
+	struct kerberos_principal_name p;
+	item = &name->name.OthName.name;
+	if (SECITEM_ItemsAreEqual(&name->name.OthName.oid,
+				  &oid_pkinit_san.oid)) {
+		memset(&p, 0, sizeof(p));
+		if (SEC_ASN1DecodeItem(arena, &p,
+				       cm_kerberos_principal_name_template,
+				       item) == SECSuccess) {
+			entry->cm_cert_principal = cm_certext_parse_principal(entry, &p);
+		}
+	}
+	if (SECITEM_ItemsAreEqual(&name->name.OthName.oid,
+				  &oid_ms_upn_name.oid)) {
+		memset(&upn, 0, sizeof(upn));
+		if (SEC_ASN1DecodeItem(arena, &upn,
+				       cm_ms_upn_name_template,
+				       item) == SECSuccess) {
+			entry->cm_cert_principal = talloc_strndup(entry,
+								  (char *)
+								  upn.data,
+								  upn.len);
+		} else
+		if (SEC_ASN1DecodeItem(arena, &upn,
+				       SEC_UTF8StringTemplate,
+				       item) == SECSuccess) {
+			entry->cm_cert_principal = talloc_strndup(entry,
+								  (char *)
+								  upn.data,
+								  upn.len);
+		}
+	}
+}
+
 static void
 cm_certext_read_san(struct cm_store_entry *entry, PLArenaPool *arena,
 		    CERTCertExtension *san_ext)
 {
 	CERTGeneralName *name, *san;
-	unsigned int i;
+	unsigned int i, j;
+	char **hostname, **email, **principal, **tmp, *s;
 	name = CERT_DecodeAltNameExtension(arena, &san_ext->value);
-	if (name != NULL) {
-		cm_log(3, "Got name(s).\n");
-	}
 	san = name;
 	i = 0;
+	talloc_free(entry->cm_cert_hostname);
+	entry->cm_cert_hostname = NULL;
+	talloc_free(entry->cm_cert_email);
+	entry->cm_cert_email = NULL;
+	talloc_free(entry->cm_cert_principal);
+	entry->cm_cert_principal = NULL;
 	while (san != NULL) {
-		cm_log(3, "SAN value %d.\n", i);
 		switch (san->type) {
 		case certDNSName:
-			cm_log(3, "dnsName\n", i);
+			s = talloc_strndup(entry, (char *) san->name.other.data,
+					   san->name.other.len);
+			entry->cm_cert_hostname = s;
+			break;
+		case certIPAddress:
+			/* binary data - see rfc5280 */
 			break;
 		case certRFC822Name:
-			cm_log(3, "email\n", i);
+			s = talloc_strndup(entry, (char *) san->name.other.data,
+					   san->name.other.len);
+			entry->cm_cert_email = s;
+			break;
+		case certOtherName:
+			/* need to parse these to recover principal names */
+			cm_certext_read_other_name(entry, arena, san);
+			break;
+		case certURI:
+		case certDirectoryName:
+		case certRegisterID:
+		case certEDIPartyName:
+		case certX400Address:
+			/* we currently don't support these */
 			break;
 		}
 		san = CERT_GetNextGeneralName(san);
@@ -163,15 +412,12 @@ cm_certext_read_extensions(struct cm_store_entry *entry, PLArenaPool *arena,
 	}
 	for (i = 0; extensions[i] != NULL; i++) {
 		if (SECITEM_ItemsAreEqual(&ku_oid->oid, &extensions[i]->id)) {
-			cm_log(1, "Extension %d: key usage.\n", i);
 			cm_certext_read_ku(entry, arena, extensions[i]);
 		}
 		if (SECITEM_ItemsAreEqual(&eku_oid->oid, &extensions[i]->id)) {
-			cm_log(1, "Extension %d: extended key usage.\n", i);
 			cm_certext_read_eku(entry, arena, extensions[i]);
 		}
 		if (SECITEM_ItemsAreEqual(&san_oid->oid, &extensions[i]->id)) {
-			cm_log(1, "Extension %d: subjectaltname.\n", i);
 			cm_certext_read_san(entry, arena, extensions[i]);
 		}
 	}
