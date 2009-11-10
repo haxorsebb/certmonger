945db172404937920e673fc45d7a4d399f6c878b
Nalin Dahyabhai <nalin.dahyabhai@pobox.com>
2009-11-10 17:12:33 -0500

    - include these in the tarball

--- tests/002-keygen/run.sh
+++ tests/002-keygen/run.sh
@@ -6,14 +6,14 @@ source "$srcdir"/functions
 initnssdb "$tmpdir"
 
 for size in 512 1024 1536 2048 3072 4096 ; do
-	certutil -d "$tmpdir" -S -g $size -n keyi$size \
-		-s "cn=T$size" -c "cn=T$size" \
-		-x -t u < /dev/urandom > /dev/null 2> /dev/null
 	cat > entry.$size <<- EOF
 	key_storage_type=NSSDB
 	key_storage_location=$tmpdir
 	key_nickname=keyi$size
+	key_gen_size=$size
 	EOF
+	$toolsdir/keygen entry.$size
+	sed -i 's,^key_gen_size.*,,g' entry.$size
 	$toolsdir/keyiread entry.$size
 done
 
@@ -22,6 +22,9 @@ for size in 512 1024 1536 2048 3072 4096 ; do
 	cat > entry.$size <<- EOF
 	key_storage_type=FILE
 	key_storage_location=$tmpdir/sample.$size
+	key_gen_size=$size
 	EOF
+	$toolsdir/keygen entry.$size
+	sed -i 's,^key_gen_size.*,,g' entry.$size
 	$toolsdir/keyiread entry.$size
 done
