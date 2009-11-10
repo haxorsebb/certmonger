4d8e0abf62f0bafe6769c91ce3a248a22356e350
Nalin Dahyabhai <nalin.dahyabhai@pobox.com>
2009-11-10 18:49:48 -0500

    - don't bother generating the key with openssl before we do it ourselves

--- tests/002-keygen/run.sh
+++ tests/002-keygen/run.sh
@@ -6,6 +6,7 @@ source "$srcdir"/functions
 initnssdb "$tmpdir"
 
 for size in 512 1024 1536 2048 3072 4096 ; do
+	# Generate a key.
 	cat > entry.$size <<- EOF
 	key_storage_type=NSSDB
 	key_storage_location=$tmpdir
@@ -13,18 +14,20 @@ for size in 512 1024 1536 2048 3072 4096 ; do
 	key_gen_size=$size
 	EOF
 	$toolsdir/keygen entry.$size
+	# Read the size.
 	sed -i 's,^key_gen_size.*,,g' entry.$size
 	$toolsdir/keyiread entry.$size
 done
 
 for size in 512 1024 1536 2048 3072 4096 ; do
-	openssl genrsa $size > sample.$size 2> /dev/null
+	# Generate a key.
 	cat > entry.$size <<- EOF
 	key_storage_type=FILE
 	key_storage_location=$tmpdir/sample.$size
 	key_gen_size=$size
 	EOF
 	$toolsdir/keygen entry.$size
+	# Read the size.
 	sed -i 's,^key_gen_size.*,,g' entry.$size
 	$toolsdir/keyiread entry.$size
 done
