02ee9a046738d6ee5fd3896b69a0684079b4da0e
Nalin Dahyabhai <nalin.dahyabhai@pobox.com>
2009-11-12 17:31:36 -0500

    - dump more information

--- src/nl-check.c
+++ src/nl-check.c
@@ -37,6 +37,104 @@ main(int argc, char **argv)
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
 
+static void
+dump_rta(struct rtattr *buf, int len)
+{
+	struct rtattr *rta;
+	for (rta = buf; RTA_OK(rta, len); rta = RTA_NEXT(rta, len)) {
+		switch (rta->rta_type) {
+		default:
+			printf(" Got an unknown attribute of length %ld.\n",
+			       (long) RTA_PAYLOAD(rta));
+			break;
+		}
+	}
+	printf(" %d leftover attribute bytes.\n", len);
+}
+
+static void
+dump_nlmsg(unsigned char *buf, int len)
+{
+	struct nlmsghdr *nlmsg;
+	struct rtmsg *rtm;
+	for (nlmsg = (struct nlmsghdr *) buf;
+	     (len > 0) && NLMSG_OK(nlmsg, (unsigned int) len);
+	     nlmsg = NLMSG_NEXT(nlmsg, len)) {
+		printf("Got a full message with payload length %ld.\n",
+		       (long ) NLMSG_PAYLOAD(nlmsg, 0));
+		rtm = NLMSG_DATA(nlmsg);
+		switch (nlmsg->nlmsg_type) {
+		case RTM_NEWLINK:
+			printf(" Got a new-link message.\n");
+			break;
+		case RTM_DELLINK:
+			printf(" Got a del-link message.\n");
+			break;
+		case RTM_GETLINK:
+			printf(" Got a get-link message.\n");
+			break;
+		case RTM_SETLINK:
+			printf(" Got a set-link message.\n");
+			break;
+		case RTM_NEWADDR:
+			printf(" Got a new-addr message.\n");
+			break;
+		case RTM_DELADDR:
+			printf(" Got a del-addr message.\n");
+			break;
+		case RTM_GETADDR:
+			printf(" Got a get-addr message.\n");
+			break;
+		case RTM_NEWROUTE:
+			printf(" Got a new-route message.\n");
+			break;
+		case RTM_DELROUTE:
+			printf(" Got a del-route message.\n");
+			break;
+		case RTM_GETROUTE:
+			printf(" Got a get-route message.\n");
+			break;
+		case RTM_NEWNEIGH:
+			printf(" Got a new-neighbor message.\n");
+			break;
+		case RTM_DELNEIGH:
+			printf(" Got a del-neighbor message.\n");
+			break;
+		case RTM_GETNEIGH:
+			printf(" Got a get-neighbor message.\n");
+			break;
+		case RTM_NEWRULE:
+			printf(" Got a new-rule message.\n");
+			break;
+		case RTM_DELRULE:
+			printf(" Got a del-rule message.\n");
+			break;
+		case RTM_GETRULE:
+			printf(" Got a get-rule message.\n");
+			break;
+		default:
+			printf(" Got an unknown message %d.\n", rtm->rtm_type);
+			rtm = NULL;
+			break;
+		}
+		if (rtm != NULL) {
+			switch (rtm->rtm_family) {
+			case AF_INET:
+				printf("  IPv4.\n");
+				break;
+			case AF_INET6:
+				printf("  IPv6.\n");
+				break;
+			default:
+				printf("  family %d.\n", rtm->rtm_family);
+				break;
+			}
+			dump_rta(RTM_RTA(nlmsg), RTM_PAYLOAD(nlmsg));
+		}
+	}
+	printf("%d leftover message bytes.\n", len);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -66,6 +164,7 @@ main(int argc, char **argv)
 			break;
 		}
 		printf("Received %d bytes.\n", len);
+		dump_nlmsg(buf, len);
 	}
 	return 0;
 }
