981a5e9a11400be2ebd00f61ba41c8703b4c112b
Nalin Dahyabhai <nalin@dahyabhai.net>
2009-11-19 13:06:10 -0500

    - take the server uri and method from arguments, returning "unconfigured" if we didn't get both - read the csr from the environment or stdin

--- src/submit-x.c
+++ src/submit-x.c
@@ -29,30 +29,9 @@
 
 #include "submit-e.h"
 
-#define REQ_DATA \
-	"-----BEGIN NEW CERTIFICATE REQUEST-----\n" \
-	"MIIC+jCCAeICAQAwFTETMBEGA1UEAxMKQm9iIEplbnNlbjCCASIwDQYJKoZIhvcN\n" \
-	"AQEBBQADggEPADCCAQoCggEBAMeBHVwuakwxp4OsPT+ooghzyr5SsyKylKJ+QP4B\n" \
-	"nQzxNSmT3O+ubtRqgv/1Rekj30Z56QMX3D9cgJfdRCmSTQ6JLpubgX1DZtgyHq4j\n" \
-	"nUtiYsObzQ83+OXlO/kUItGVJa2308+rAQ6FkpI8S0WwiXgfZIZmbIjghkpfj+XT\n" \
-	"PtjVsBwKVxr39++Hq0zA+1YzKPZEe+mU0C8s7zh0tzEiXVEcOnwLL25QpEVDUVxd\n" \
-	"HKHBfnVOmsN9ju9BO48b+zIIB5qtSSir+jTs9+JqRX00nsPXVonhXMHOxOjc9pMJ\n" \
-	"V3D8wIfXzeW10xNA3YYCi66XiZTicfsFV8Z47Mrq0yytCe0CAwEAAaCBnzATBgkq\n" \
-	"hkiG9w0BCRQxBhMEQmFiczCBhwYJKoZIhvcNAQkOMXoweDB2BgNVHREBAQAEbDBq\n" \
-	"gRBiYWJzQGV4YW1wbGUuY29toCMGCisGAQQBgjcUAgOgFQwTYmplbnNlbkBFWEFN\n" \
-	"UExFLkNPTaAxBgYrBgEFAgKgJzAloA0bC0VYQU1QTEUuQ09NoRQwEqADAgEBoQsw\n" \
-	"CRsHYmplbnNlbjANBgkqhkiG9w0BAQsFAAOCAQEAOi7Jd5CTTZvCye9iep8R7hMJ\n" \
-	"DK45btHEiNB9SEqhVcpIhTPh+6Q/1NtuLYGbk0MSe8ocqCO0g2x9kANsVTH4FBOz\n" \
-	"RU9nbucOQpwrWtmOjzJAth04WSIhVQfTTL0ihTxRS2QmPrs/W3rNTWbTsKIwv7ta\n" \
-	"8REWElKQjvkt9ejWbIsp70eOaQKFjgA5W69wjguKdRbs9H66CqIdlYK4GMpOO8pH\n" \
-	"7keIGHRzSc3SEs0H2RchLgaX922o0CDsg28mbvUDFL2bbQy0kqyzKGdMuwwMMlO9\n" \
-	"L7bOVSOHtpWw2ScQlN+Y3ljPAOZ2F1FdloCtozBJju5CyIrGOqCj8kiOWh3zSw==\n" \
-	"-----END NEW CERTIFICATE REQUEST-----\n"
-
 int
 main(int argc, char **argv)
 {
-	const char *uri;
 	xmlrpc_env xenv;
 	xmlrpc_server_info *server;
 	xmlrpc_client *client;
@@ -61,22 +40,81 @@ main(int argc, char **argv)
 	struct xmlrpc_curl_xportparms xparams;
 	xmlrpc_client_transport *xtransport;
 	xmlrpc_bool boo;
-	int i, ret;
-	const char *s;
+	int i, c, ret;
+	const char *uri = NULL, *method = NULL, *s;
+	char *csr, *p, buf[BUFSIZ];
 
 	memset(&xenv, 0, sizeof(xenv));
 	xmlrpc_env_init(&xenv);
 	xmlrpc_client_setup_global_const(&xenv);
 
-	uri = "http://localhost:51235/"; /* XXX - certmaster default */
+	while ((c = getopt(argc, argv, "s:m:")) != -1) {
+		switch (c) {
+		case 's':
+			uri = optarg;
+			break;
+		case 'm':
+			method = optarg;
+			break;
+		default:
+			fprintf(stderr,
+				"Usage: %s [-s serverURI] [-m method]\n"
+				"Examples:\n"
+				"           -s http://localhost:51235/\n"
+				"           -m wait_for_cert\n",
+				strchr(argv[0], '/') ?
+				strrchr(argv[0], '/') + 1 :
+				argv[0]);
+			return CM_STATUS_UNCONFIGURED;
+			break;
+		}
+	}
+	if ((uri == NULL) || (method == NULL)) {
+		return CM_STATUS_UNCONFIGURED;
+	}
 	server = xmlrpc_server_info_new(&xenv, uri);
 	results = NULL;
-	ret = STATUS_UNREACHABLE;
+	ret = CM_STATUS_UNREACHABLE;
+	csr = getenv(CM_SUBMIT_CSR_ENV);
+	if (csr == NULL) {
+		while (fgets(buf, sizeof(buf), stdin) != NULL) {
+			if (csr == NULL) {
+				csr = strdup(buf);
+				if (csr == NULL) {
+					return CM_STATUS_UNREACHABLE;
+				}
+			} else {
+				p = malloc(strlen(csr) + sizeof(buf));
+				if (p == NULL) {
+					return CM_STATUS_UNREACHABLE;
+				}
+				memcpy(stpcpy(p, csr), buf, sizeof(buf));
+				free(csr);
+				csr = p;
+			}
+		}
+	}
+	if (strcmp(method, "wait_for_cert") == 0) {
+		/* certmaster rewrites the incoming request to its cache
+		 * previously-received requests, and in doing so uses a
+		 * different PEM header than the one we default to using.  So
+		 * turn any "NEW CERTIFICATE REQUEST" notes into "CERTIFICATE
+		 * REQUEST". */
+		while ((p = strstr(csr, "NEW CERTIFICATE REQUEST")) != NULL) {
+			memmove(p, p + 4, strlen(p + 4) + 1);
+		}
+	}
 
 	if (server != NULL) {
-		xmlrpc_server_info_set_user(&xenv, server, "", "");
+		xmlrpc_server_info_disallow_auth_basic(&xenv, server);
 		if (xenv.fault_occurred) {
-			printf("Fault %d: (%s).\n",
+			printf("Fault %d turning off basic auth: (%s).\n",
+			       xenv.fault_code, xenv.fault_string);
+			xmlrpc_env_clean(&xenv);
+		}
+		xmlrpc_server_info_disallow_auth_negotiate(&xenv, server);
+		if (xenv.fault_occurred) {
+			printf("Fault %d turning off negotiate auth: (%s).\n",
 			       xenv.fault_code, xenv.fault_string);
 			xmlrpc_env_clean(&xenv);
 		}
@@ -107,8 +145,7 @@ main(int argc, char **argv)
 				results = NULL;
 				params = xmlrpc_array_new(&xenv);
 				if (params != NULL) {
-					req = xmlrpc_string_new(&xenv,
-								REQ_DATA);
+					req = xmlrpc_string_new(&xenv, csr);
 					if (req != NULL) {
 						xmlrpc_array_append_item(&xenv,
 									 params,
@@ -117,7 +154,7 @@ main(int argc, char **argv)
 					xmlrpc_client_call2(&xenv,
 							    client,
 							    server,
-							    "wait_for_cert",
+							    method,
 							    params,
 							    &results);
 					if (xenv.fault_occurred) {
@@ -189,7 +226,7 @@ main(int argc, char **argv)
 					} else {
 						printf("Status: %d.\n", boo);
 						if (boo) {
-							ret = STATUS_ISSUED;
+							ret = CM_STATUS_ISSUED;
 						}
 						if (i > 2) {
 							/* The next two are our
