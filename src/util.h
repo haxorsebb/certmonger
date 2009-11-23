77b8900cf4f46f284bf716e4b0d8c0fe620562e7
Nalin Dahyabhai <nalin.dahyabhai@pobox.com>
2009-11-23 13:42:48 -0500

    - add config file parsing from John and Rob

---
+++ src/util.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2009 Red Hat, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef util_h
+#define util_h
+
+char *read_config_file(const char *filename);
+char *get_config_entry(char *data, const char *section, const char *key);
+char *get_ipa_server(char *data);
+char *get_ipa_realm(char *data);
+
+#endif
