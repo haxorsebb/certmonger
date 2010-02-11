379a8fd2e6c65aa1b011eccb6dd71a19f5677dc0
Nalin Dahyabhai <nalin@dahyabhai.net>
2010-02-10 21:04:23 -0500

    - clean up the test a bit; remove the database before checking on certsave

--- tests/011-dbinit/run.sh
+++ tests/011-dbinit/run.sh
@@ -5,6 +5,7 @@ cd "$tmpdir"
 source "$srcdir"/functions
 
 for scheme in "" dbm sql ; do
+	echo '['Generating key${scheme:+ \($scheme\)}.']'
 	rm -fr $tmpdir/${scheme}db
 	mkdir -p $tmpdir/${scheme}db
 	cat > entry <<- EOF
@@ -13,37 +14,15 @@ for scheme in "" dbm sql ; do
 	key_storage_type=NSSDB
 	key_storage_location=${scheme:+${scheme}:}$tmpdir/${scheme}db
 	key_nickname=Test
-	cert_storage_type=NSSDB
-	cert_storage_location=${scheme:+${scheme}:}$tmpdir/${scheme}db
-	cert_nickname=Test
-	cert=-----BEGIN CERTIFICATE-----
-	 MIIC3jCCAcagAwIBAgIBAzANBgkqhkiG9w0BAQsFADASMRAwDgYDVQQDEwdwaWxs
-	 Ym94MB4XDTEwMDIxMDIyMDMzOVoXDTEwMDMxMjIyMDMzOVowEjEQMA4GA1UEAxMH
-	 cGlsbGJveDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMQOQn5USR/Q
-	 Gp2230fks3ZOjkF5VHxwLziS9rc+AFZ8UZrXMidnkhso9Eqp74CaJ+KhJI2F62wm
-	 SerBztRAVb8T98+dXUvgYIXE6OxB0ITCMMvdJZFKs5hek2Xd6uiulCegqsNOD1qy
-	 llBNLtWDoZqgXuEKfeQmUR6qUGqhVFfmL7qKmIOWN+lSswhkQrrGy3oSNVU5KWYM
-	 d7bkrKWEze8ksWgNOwDFQ2pQibYljywEfBZaLegeoASygK3yl6dVjioQmkHBk8Z1
-	 fRLnMs8TRT7NwgsWFkKi04SGkn/VpVKZ9piMJCpYhQWIy0U2ib0nBaYec2ReFQ6r
-	 2du1UMmkwXECAwEAAaM/MD0wFQYDVR0RAQEABAswCYIHcGlsbGJveDAWBgNVHSUB
-	 AQAEDDAKBggrBgEFBQcDATAMBgNVHRMBAf8EAjAAMA0GCSqGSIb3DQEBCwUAA4IB
-	 AQA9dqnzVzblb0PKdiMOzLksEqFridaRVOB/hK4WHeSJQsCk6a151Fli1uX3/QHJ
-	 vRXH0P6i8eMblQ20J4IpZuO9aBLW1go8vPQM8/gD5dXUVm57sqsJlvxjKbnHplGi
-	 w8KKasuYMHGOI0M//MR84LI7Nd/JIu+9In9Y+qRj91saBIgHDKeiHQtzWdehNC+2
-	 e3gdWc74hx26gXRO6bNE5CZExnVULNkDOsPh/nr4Qwwx+BOn4DdU8tbRvUbvjjzQ
-	 koiuvyXyTlj1E8JcT6q4P3YbCn4PTlF8xZK9+XdUzOA6HUlz2Q/ysjIQMHe6zapD
-	 8Vw+Zwf78Wg6L4tcAJ6Y4W/Z
-	 -----END CERTIFICATE-----
 	EOF
-	echo Generating key${scheme:+ \($scheme\)}.
 	$toolsdir/keygen entry
-	certutil -K -d ${scheme:+${scheme}:}$tmpdir/${scheme}db | sed -re 's,rsa .* Test,rsa PRIVATE KEY Test,g' -e 's,[ \t]+, ,g'
+	certutil -K -d ${scheme:+${scheme}:}$tmpdir/${scheme}db 2>&1 | sed -re 's,rsa .* Test,rsa PRIVATE-KEY Test,g' -e 's,[ \t]+, ,g'
+
+	echo '['Saving certificate${scheme:+ \($scheme\)}.']'
+	rm -fr $tmpdir/${scheme}db
+	mkdir -p $tmpdir/${scheme}db
 	cat > entry <<- EOF
 	state=NEED_TO_SAVE_CERT
-	key_type_default=1
-	key_storage_type=NSSDB
-	key_storage_location=${scheme:+${scheme}:}$tmpdir/${scheme}db
-	key_nickname=Test
 	cert_storage_type=NSSDB
 	cert_storage_location=${scheme:+${scheme}:}$tmpdir/${scheme}db
 	cert_nickname=Test
@@ -66,9 +45,8 @@ for scheme in "" dbm sql ; do
 	 8Vw+Zwf78Wg6L4tcAJ6Y4W/Z
 	 -----END CERTIFICATE-----
 	EOF
-	echo Saving certificate${scheme:+ \($scheme\)}.
-	certutil -L -d ${scheme:+${scheme}:}$tmpdir/${scheme}db | sed -re 's,[ \t]+, ,g'
-	echo OK
 	$toolsdir/certsave entry
+	echo OK
+	certutil -L -d ${scheme:+${scheme}:}$tmpdir/${scheme}db 2>&1 | sed -re 's,[ \t]+, ,g'
 done
-echo Test complete.
+echo '['Test complete.']'
