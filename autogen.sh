57457695bf0f523af1a0b83e0813229d1d8f20b7
Nalin Dahyabhai <nalin@dahyabhai.net>
2011-07-13 01:00:30 -0400

    - give everything a workout

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,4 @@ CFLAGS=`rpm --eval '%{optflags} -Wall -Wextra -Wno-unused-parameter -g3 -O0'`
 export CFLAGS
 set -x
 autoreconf -i -f
-./configure --prefix=/usr --sysconfdir=/etc --enable-systemd --enable-sysvinit --with-tmpdir=/var/run/certmonger  --localstatedir=/var --enable-maintainer-mode "$@"
+./configure --prefix=/usr --sysconfdir=/etc --enable-systemd --enable-sysvinit=/etc/rc.d/init.d --with-tmpdir=/var/run/certmonger  --localstatedir=/var --enable-maintainer-mode "$@"
