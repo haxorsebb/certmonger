fcb63824a85b37b3ef60f0f773388264fd31de4b
Nalin Dahyabhai <nalin@dahyabhai.net>
2011-08-11 01:11:50 -0400

    - a sample to print info about the tokens we have

---
+++ src/toklist.c
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2011 Red Hat, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <nss.h>
+#include <nssb64.h>
+#include <cert.h>
+#include <keyhi.h>
+#include <keythi.h>
+#include <pk11pub.h>
+#include <prerror.h>
+
+#include "pin.h"
+
+int
+main(int argc, char **argv)
+{
+	PLArenaPool *arena;
+	PK11SlotList *slotlist;
+	PK11SlotListElement *sle;
+	CK_MECHANISM_TYPE mech = 0;
+	CK_TOKEN_INFO info;
+	const char *dbdir = "/etc/pki/nssdb", *token;
+	SECStatus error;
+	int c;
+
+	while ((c = getopt(argc, argv, "d:m:")) != -1) {
+		switch (c) {
+		case 'd':
+			dbdir = optarg;
+			break;
+		case 'm':
+			mech = atol(optarg);
+			break;
+		}
+	}
+	printf("Mechanism %ld:\n", (long) mech);
+
+	/* Open the database. */
+	error = NSS_Init(dbdir);
+	if (error != SECSuccess) {
+		printf("Unable to open NSS database '%s'.\n", dbdir);
+		_exit(CM_STATUS_ERROR_INITIALIZING);
+	}
+
+	/* Allocate a memory pool. */
+	arena = PORT_NewArena(sizeof(double));
+	if (arena == NULL) {
+		printf("Out of memory opening database '%s'.\n", dbdir);
+		if (NSS_Shutdown() != SECSuccess) {
+			printf("Error shutting down NSS.\n");
+		}
+		_exit(CM_STATUS_ERROR_INITIALIZING);
+	}
+
+	/* Find the tokens that we might use for key storage. */
+	slotlist = PK11_GetAllTokens(mech, PR_FALSE, PR_FALSE, NULL);
+	if (slotlist == NULL) {
+		if (NSS_Shutdown() != SECSuccess) {
+			printf("Error shutting down NSS.\n");
+		}
+		_exit(CM_STATUS_ERROR_NO_TOKEN);
+	}
+
+	for (sle = slotlist->head;
+	     ((sle != NULL) && (sle->slot != NULL));
+	     sle = sle->next) {
+		/* Read the token's name. */
+		token = PK11_GetTokenName(sle->slot);
+		if (token != NULL) {
+			printf("Found token '%s'.\n", token);
+		} else {
+			printf("Found unnamed token.\n");
+		}
+		memset(&info, 0, sizeof(info));
+		if (PK11_GetTokenInfo(sle->slot, &info) == SECSuccess) {
+			printf("\tFlags = %08lx\n", info.flags);
+			printf("\tPIN Length = %lu..%lu\n", info.ulMinPinLen,
+			       info.ulMaxPinLen);
+		}
+		/* Now log in, if we have to. */
+		if (PK11_NeedLogin(sle->slot)) {
+			printf("\tToken requires login.\n");
+		} else {
+			printf("\tToken does not require login.\n");
+		}
+
+		/* If this was the last token, stop walking. */
+		if (sle == slotlist->tail) {
+			break;
+		}
+	}
+
+	PK11_FreeSlotList(slotlist);
+
+	PORT_FreeArena(arena, PR_TRUE);
+
+	return 0;
+}
