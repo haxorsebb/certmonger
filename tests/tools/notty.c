0b48599de5ac4c931c4d50d86d451a188fa4bcab
Nalin Dahyabhai <nalin@dahyabhai.net>
2012-01-31 13:15:43 -0500

    - check for argc >= 2

--- tests/tools/notty.c
+++ tests/tools/notty.c
@@ -1,8 +1,12 @@
 #include <sys/ioctl.h>
+#include <errno.h>
 #include <unistd.h>
 int
 main(int argc, char **argv)
 {
+	if (argc < 2) {
+		return EINVAL;
+	}
 	ioctl(STDIN_FILENO, TIOCNOTTY);
 	return execvp(argv[1], argv + 1);
 }
