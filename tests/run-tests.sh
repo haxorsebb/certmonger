1b6f4dedd052b135290117162c84f8584d057366
Nalin Dahyabhai <nalin@dahyabhai.net>
2014-02-03 22:57:21 -0500

    Accept alternate results

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -69,10 +69,21 @@ for testid in "$@" $subdirs ; do
 				cp "$tmpdir"/errors "$builddir"/"$testid"/actual.err
 			else
 				stat=1
-				echo "FAIL"
-				diff -u expected.out "$tmpfile" | sed s,"^\+\+\+ $tmpfile","+++ actual",g
-				cp $tmpfile "$builddir"/"$testid"/actual.out
-				cp "$tmpdir"/errors "$builddir"/"$testid"/actual.err
+				for i in `seq 2 4`; do
+					if cmp "$tmpfile" expected.out.$i 2> /dev/null ; then
+						stat=0
+						echo "OK"
+						cp $tmpfile "$builddir"/"$testid"/actual.out
+						cp "$tmpdir"/errors "$builddir"/"$testid"/actual.err
+						break
+					fi
+				done
+				if test $stat -eq 1 ; then
+					echo "FAIL"
+					diff -u expected.out "$tmpfile" | sed s,"^\+\+\+ $tmpfile","+++ actual",g
+					cp $tmpfile "$builddir"/"$testid"/actual.out
+					cp "$tmpdir"/errors "$builddir"/"$testid"/actual.err
+				fi
 			fi
 		else
 			echo "Running test "$testid"."
