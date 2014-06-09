f5aec57e943e75d26bb6559357285fca7a653a0a
Nalin Dahyabhai <nalin@dahyabhai.net>
2014-06-09 15:58:34 -0400

    Auto-config the local signer helper, add man page

--- src/store-files.c
+++ src/store-files.c
@@ -1994,7 +1994,7 @@ cm_store_get_all_cas(void *parent)
 	if (glob(path, 0, NULL, &globs) != 0) {
 		globs.gl_pathc = 0;
 	}
-	ret = talloc_array_ptrtype(parent, ret, globs.gl_pathc + 5);
+	ret = talloc_array_ptrtype(parent, ret, globs.gl_pathc + 6);
 	if (ret != NULL) {
 		for (i = 0, j = 0; i < globs.gl_pathc; i++) {
 			p = globs.gl_pathv[i];
@@ -2102,6 +2102,26 @@ cm_store_get_all_cas(void *parent)
 			ret[j]->cm_ca_type = cm_ca_external;
 			ret[j]->cm_ca_external_helper = talloc_strdup(ret[j],
 								      CM_DOGTAG_IPA_RENEW_AGENT_HELPER_PATH);
+			j++;
+		}
+#endif
+#ifdef WITH_LOCAL
+		/* Make sure we get at least 1 dogtag-ipa-renew-agent entry. */
+		for (k = 0; k < j; k++) {
+			if ((ret[k]->cm_ca_type == cm_ca_external) &&
+			    (strcmp(ret[k]->cm_nickname,
+				    CM_LOCAL_CA_NAME) == 0)) {
+				break;
+			}
+		}
+		if (k == j) {
+			ret[j] = cm_store_ca_new(ret);
+			ret[j]->cm_busname = cm_store_ca_next_busname(ret[j]);
+			ret[j]->cm_nickname = talloc_strdup(ret[j],
+							    CM_LOCAL_CA_NAME);
+			ret[j]->cm_ca_type = cm_ca_external;
+			ret[j]->cm_ca_external_helper = talloc_strdup(ret[j],
+								      CM_LOCAL_HELPER_PATH);
 			j++;
 		}
 #endif
