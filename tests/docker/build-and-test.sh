8fedd27a945c8b3f54d28ab91213d90f8436b925
Nalin Dahyabhai <nalin@redhat.com>
2017-02-18 21:11:23 -0500

    Add some docker build configurations
    Add a script for generating Dockerfiles for a range of build targets,
    and a script for running our self-tests on those build targets.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

---
+++ tests/docker/build-and-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+set -e
+if test -s /etc/os-release; then
+	source /etc/os-release
+else
+	if test -s /etc/system-release ; then
+		PRETTY_NAME=`cat /etc/system-release`
+	else
+		PRETTY_NAME=unknown
+	fi
+fi
+# Print PRETTY_NAME periodically, so it's easier to know where we are if we're
+# paging through a log of results from multiple containers.
+echo '['"${PRETTY_NAME}"']'
+rm -fr /build/certmonger
+cp -a /source /build/certmonger
+cd /build/certmonger
+echo '['"${PRETTY_NAME}"']'
+export CFLAGS="-Wall -Wextra -Wno-unused-parameter"
+./configure --prefix=/usr --sysconfdir=/etc --with-tmpdir=/var/run/certmonger  --localstatedir=/var --disable-maintainer-mode --enable-srv-location --disable-systemd --disable-sysvinit
+echo '['"${PRETTY_NAME}"']'
+make all
+echo '['"${PRETTY_NAME}"']'
+make check
