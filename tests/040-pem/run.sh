08bd8ae46f948064806a26cd267ec6c75cc67603
Rob Crittenden <rcritten@redhat.com>
2022-03-17 16:20:24 -0400

    Replace DER-encoded test file with a base64-encoded one
    Some build systems will not allow patches that contain
    binaries so switch to a base64-encoded file instead.
    
    Related: https://bugzilla.redhat.com/show_bug.cgi?id=1492112
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>

--- tests/040-pem/run.sh
+++ tests/040-pem/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e
 
 cd "$tmpdir"
+cp -p "$srcdir"/040-pem/bad.* $tmpdir
+base64 -d < "$tmpdir"/bad.isrg-root-x1-cross-signed.der.b64 > "$tmpdir"/bad.isrg-root-x1-cross-signed.der
+rm -f "$tmpdir"/bad.isrg-root-x1-cross-signed.der.b64
 
 for good in "$srcdir"/040-pem/good.* ; do
 	if ! "$toolsdir"/pem "$good" ; then
 		exit 1
 	fi
 done
-for bad in "$srcdir"/040-pem/bad.* bad.notfound; do
+for bad in "$tmpdir"/bad.* bad.notfound; do
 	if "$toolsdir"/pem "$bad" > /dev/null; then
 		echo unexpected success with `basename "$bad"`
 		exit 1
