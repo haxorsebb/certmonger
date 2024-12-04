e1637da7484a97131ce37140aca6ef2e1402f590
Rob Crittenden <rcritten@redhat.com>
2024-12-04 18:29:30 +0000

    Add tests for the expanded dogtag requests through IPA API
    This extends the current 019-dparse tests to include IPA json output
    validation.
    
    Fixes: https://pagure.io/certmonger/issue/284
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>

--- tests/019-dparse/run.sh
+++ tests/019-dparse/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 count=0
-for role in agent end-user ; do
-for good in good.profileSubmit* ; do
+for role in agent end-entity ; do
+for good in good.profileSubmit*.xml ; do
 	$toolsdir/dparse submit $role $good
 	count=`expr $count + 1`
 done
@@ -9,19 +9,19 @@ for good in good.profileReview* ; do
 	$toolsdir/dparse review $role $good
 	count=`expr $count + 1`
 done
-for good in good.checkRequest* ; do
+for good in good.checkRequest*.xml ; do
 	$toolsdir/dparse check $role $good
 	count=`expr $count + 1`
 done
-for good in good.displayCertFromRequest* ; do
+for good in good.displayCertFromRequest*.xml ; do
 	$toolsdir/dparse fetch $role $good
 	count=`expr $count + 1`
 done
-for good in good.profileList* ; do
+for good in good.profileList*.xml ; do
 	$toolsdir/dparse profiles $role $good
 	count=`expr $count + 1`
 done
-for bad in bad.profileSubmit* ; do
+for bad in bad.profileSubmit*.xml ; do
 	$toolsdir/dparse submit $role $bad
 	count=`expr $count + 1`
 done
@@ -33,11 +33,42 @@ for bad in bad.profileProcess* ; do
 	$toolsdir/dparse approve $role $bad
 	count=`expr $count + 1`
 done
-for bad in bad.checkRequest* ; do
+for bad in bad.checkRequest*.xml ; do
 	$toolsdir/dparse check $role $bad
 	count=`expr $count + 1`
 done
 for bad in bad.displayCertFromRequest* ; do
+	$toolsdir/dparse fetch $role $bad
+	count=`expr $count + 1`
+done
+done
+
+for role in json ; do
+for good in good.profileSubmit*.json ; do
+	$toolsdir/dparse submit $role $good
+	count=`expr $count + 1`
+done
+for good in good.checkRequest*.json ; do
+	$toolsdir/dparse check $role $good
+	count=`expr $count + 1`
+done
+for good in good.displayCertFromRequest*.json ; do
+	$toolsdir/dparse fetch $role $good
+	count=`expr $count + 1`
+done
+for good in good.profileList*.json ; do
+	$toolsdir/dparse profiles $role $good
+	count=`expr $count + 1`
+done
+for bad in bad.profileSubmit*.json ; do
+	$toolsdir/dparse submit $role $bad
+	count=`expr $count + 1`
+done
+for bad in bad.checkRequest*.json ; do
+	$toolsdir/dparse check $role $bad
+	count=`expr $count + 1`
+done
+for bad in bad.displayCertFromRequest*.json ; do
 	$toolsdir/dparse fetch $role $bad
 	count=`expr $count + 1`
 done
